f206c2854f5a6ee37c3a44e19f5d1e4585e642fb
Veneta Stefanova <venetastefanovaa@gmail.com>
2019-02-09 12:02:46 +0200

    not pretty but working sensors API

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,8 @@
-FROM node:10
-
-# Create app directory
-WORKDIR /usr/src/sensors-data-api
-
-# Install app dependencies
-# A wildcard is used to ensure both package.json AND package-lock.json are copied
-# where available (npm@5+)
-COPY package*.json ./
-
+FROM node:latest
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+COPY package.json /usr/src/app/
 RUN npm install
-# If you are building your code for production
-# RUN npm install --only=production
-
-# Bundle app source
-COPY . .
-
+COPY . /usr/src/app
 EXPOSE 3000
-CMD [ "npm", "start" ]
+CMD [ “npm”, “start” ]
--- server.js
+++ server.js
@@ -1,26 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = 3000
 const axios = require('axios');
-// var tokenUrl = "https://opendata.hopefully.works/api/signup";
-// var requestTokenUser = {
-//   "email": "venetastefanovaa@gmail.com", 
-//   "password": "cookieDough*"
-// }
-// var requestTokenHeaders ={
-//     headers: {
-//       "Content-type": "application/json"
-//     } 
-// }
+const bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-//register and gets a token
-// axios.post(tokenUrl,requestTokenUser, requestTokenHeaders)
-//   .then(response => {
-//     console.log(response);   
-//   })
-//   .catch(error => {
-//     console.log(error);
-// });
+//body parser setup
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjgsImVtYWlsIjoidmVuZXRhc3RlZmFub3ZhYUBnbWFpbC5jb20iLCJpYXQiOjE1NDk2NDM2MjJ9.giTNvsw7hxGAix4THRuAhDrkh5ilKQ1DeUTc_hs4UXA";
 var eventsHeader = {
@@ -29,18 +16,50 @@ var eventsHeader = {
     } 
 }
 
-//gets the data from events API every 1h
+//database connection setup
+var mongoDB = 'mongodb://mongo/sensors_data';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));//Bind connection to error event (to get notification of connection errors)
+
+// database schema
+const dataEntrySchema = new mongoose.Schema({
+  date: String,
+  sensor1: Number,
+  sensor2: Number,
+  sensor3: Number,
+  sensor4: Number
+});
+
+// Compile model from schema
+var Entry = mongoose.model('SomeEntry', dataEntrySchema);
+
+//gets the data from events API every 1h and save it to the database
 setInterval(function() {
     axios.get("https://opendata.hopefully.works/api/events", eventsHeader)
-    .then(response=>{
+    .then(response=>{ 
       console.log(response.data);
+      //save the respose to DB
+      new Entry({
+        date: response.data.date,
+        sensor1: response.data.sensor1,
+        sensor2: response.data.sensor2,
+        sensor3: response.data.sensor3,
+        sensor4: response.data.sensor4
+      }).save(function (error) {
+        if (error){
+          console.log(error)
+        }
+      });
     })
     .catch(error=>{
       console.log(error)
     })
 },1000 * 60 * 60);
+//},1000 * 60 * 60);
 
 
-app.get('/', (req, res) => res.send('working!'))
+app.get('/', (req, res) => res.send('working!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`The surver is running on port ${port}!`))
